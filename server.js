05d81c2020cb649c6497220f97491c72d5fa4d53
thiizz <afffvitu@gmail.com>
2025-06-08 12:37:08 -0300

    Increase JSON payload limit in server middleware to 50mb

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 // Serve static files from the React build
 app.use(express.static(path.join(__dirname, 'build')));
